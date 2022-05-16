a10d53a1d66f72da385f91841affe4e2b1dc1063
Kangdyu <kangdyu99@gmail.com>
2022-05-16 22:28:32 +0900

    feat: home todo data

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -18,9 +18,37 @@ function HomePage() {
     },
   );
 
-  console.log(queries);
+  const isLoading = queries.some(query => query.isLoading);
+  if (isLoading) return <main>Loading...</main>;
 
-  return <PageContentContainer title='Home'>HomePage</PageContentContainer>;
+  return (
+    <PageContentContainer title='Home'>
+      <h2>수업</h2>
+      <ul>
+        {queries.map(query =>
+          query.data.data.videos
+            .sort((a, b) => new Date(a.due) - new Date(b.due))
+            .map(video => (
+              <li key={video.id}>
+                {video.title} {video.due}
+              </li>
+            )),
+        )}
+      </ul>
+      <h2>과제</h2>
+      <ul>
+        {queries.map(query =>
+          query.data.data.assignments
+            .sort((a, b) => new Date(a.due) - new Date(b.due))
+            .map(assignment => (
+              <li key={assignment.id}>
+                {assignment.title} {assignment.due}
+              </li>
+            )),
+        )}
+      </ul>
+    </PageContentContainer>
+  );
 }
 
 export default HomePage;
