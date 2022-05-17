5c89fd6e446e485e877366f34dfb6b2a89d9efa3
chadh0829 <donghoon0829@gmail.com>
2022-05-17 22:26:04 +0900

    feat: getNotice 기능 추가

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -1,6 +1,7 @@
 import { MOCKUP_COURSES } from './mockup/courses';
 import { MOCKUP_NOTICES } from './mockup/notices';
 import { MOCKUP_TODOS } from './mockup/todos';
+import { getNotice } from './getNotice';
 
 function mockupListener(msg, sendResponse) {
   if (msg.type === 'courses') {
@@ -35,7 +36,16 @@ function messageListener(msg, sender, sendResponse) {
     } else if (msg.type === 'todos') {
       sendResponse({ data: `${msg.course} todos` });
     } else if (msg.type === 'notices') {
-      sendResponse({ data: `${msg.course} notices` });
+      let response;
+      let noticeData;
+      getNotice(msg.courseId).then(res => (noticeData = res));
+      if (noticeData === undefined) {
+        response = { data: 'unknown' };
+      } else {
+        response = noticeData;
+      }
+      console.log(response);
+      sendResponse(response);
     }
   }
 
---
+++ src/chrome/getNotice.js
@@ -0,0 +1,22 @@
+export async function getNotice(cid) {
+  let arr = [];
+  let ENDPOINT =
+    'https://canvas.skku.edu/api/v1/courses/' +
+    String(cid) +
+    '/discussion_topics?only_announcements=true&per_page=40&page=1&filter_by=all&no_avatar_fallback=1&include[]=sections_user_count&include[]=sections';
+  const response = await fetch(ENDPOINT, {
+    headers: { Accept: 'application/json' },
+  });
+  const responseJson = await response.json();
+  for (let key in Object.keys(responseJson)) {
+    let data = responseJson[key];
+    let notice = {};
+    notice.id = data.id;
+    notice.title = data.title;
+    notice.courseName = null;
+    notice.postedDate = new Date(data.created_at);
+    notice.url = data.url;
+    arr.push(notice);
+  }
+  return arr;
+}
