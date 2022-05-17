d80ae0961dd99cdb1a132bcd4b0348d67090e06f
hohyun Na <skghgus9@gmail.com>
2022-05-17 14:59:34 +0900

    reflected review, and add async because of promise object

--- src/chrome/getTodo.js
+++ src/chrome/getTodo.js
@@ -1,12 +1,20 @@
-export function todoAPI(courseID) {
-  let userID = getuserID(courseID);
+export async function getTodo(courseID) {
+  let userID;
+  await getUserID(courseID).then(appdata => {
+    userID = appdata;
+  });
   console.log(userID);
 
-  let authToken = getcookie();
+  let authToken;
+  await getCookie('xn_api_token').then(appdata => {
+    authToken = appdata;
+  });
   console.log(authToken);
+
+  //implement with userID, authToken
 }
 
-async function getuserID(courseID) {
+async function getUserID(courseID) {
   let basicurl = 'https://canvas.skku.edu/api/v1/courses/' + courseID;
   const response = await fetch(basicurl, {
     headers: { Accept: 'application/json' },
@@ -18,13 +26,14 @@ async function getuserID(courseID) {
   return userID;
 }
 
-async function getcookie() {
+async function getCookie(token_name) {
   const cookies = await chrome.cookies.getAll({ domain: 'canvas.skku.edu' });
-  console.log(cookies);
+
   let token;
   for (let i = 0; i < cookies.length; i++) {
-    if (cookies[i].name === 'xn_api_token') {
+    if (cookies[i].name === token_name) {
       token = cookies[i].value;
+      break;
     }
   }
   return token;
